b1ff8e0c4a055370d4185e7820d1c212d0b00b82
leedonggyu <zkfmapf123@naver.com>
2025-06-02 22:26:10 +0900

    wip-d599bc65

--- main.go
+++ main.go
@@ -11,8 +11,6 @@ func parameterInit() models.TerraDriftInputParams {
 
 	return models.TerraDriftInput(
 		models.WithConcurreny(os.Getenv("INPUT_CONCURRENCY")),
-		models.WithIsUseTerraformPath(os.Getenv("INPUT_WITH_IS_USE_TERRAFORM_PATH")),
-		models.WithIsUseTerragruntPath(os.Getenv("INPUT_WITH_IS_USE_TERRAGRUNT_PATH")),
 		models.WithSlackChannel(os.Getenv("INPUT_SLACK_CHANNEL")),
 		models.WithSlackToken(os.Getenv("INPUT_SLACK_TOKEN")),
 	)
@@ -24,5 +22,5 @@ func main() {
 	fmt.Println(params)
 
 	// Hello World 메시지 출력
-	fmt.Printf("Hello, %s!\n", name)
+	fmt.Println("%s %s %d", params.SlackParams.Token, params.SlackParams.Channel, params.Concurrency)
 }
--- models/model.go
+++ models/model.go
@@ -3,10 +3,7 @@ package models
 import "strconv"
 
 type TerraDriftInputParams struct {
-	IsUseTerraformPath  string // Terraform Project Path
-	IsUseTerragruntPath string // Terragrunt Project Path
-	Concurrency         int    // Concurrency Level
-
+	Concurrency int              // Concurrency Level
 	SlackParams SlackInputParams // Slack Channel
 }
 
@@ -26,28 +23,6 @@ func TerraDriftInput(opts ...TerraDriftInputOption) TerraDriftInputParams {
 	}
 
 	return *o
-}
-
-func WithIsUseTerraformPath(v string) TerraDriftInputOption {
-	return func(o *TerraDriftInputParams) {
-
-		// if len(v) == 0 {
-		// 	v = nil
-		// }
-
-		o.IsUseTerraformPath = v
-	}
-}
-
-func WithIsUseTerragruntPath(v string) TerraDriftInputOption {
-	return func(o *TerraDriftInputParams) {
-
-		// if len(v) == 0 {
-		// 	v = nil
-		// }
-
-		o.IsUseTerragruntPath = v
-	}
 }
 
 func WithConcurreny(v string) TerraDriftInputOption {
