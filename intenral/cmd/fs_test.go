8c6aa7732bce2970c1b1786efdde3b9d40a0b60f
leedonggyu <zkfmapf123@naver.com>
2025-06-03 22:06:21 +0900

    wip-72def805

--- intenral/cmd/fs_test.go
+++ intenral/cmd/fs_test.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,17 +11,4 @@ func Test_Shell(t *testing.T) {
 	_, err := Exec("ls", "-al")
 
 	assert.Equal(t, err, nil)
-}
-
-func Test_CurrentDir(t *testing.T) {
-
-	tgPath, tfPath := map[string]bool{}, map[string]bool{}
-
-	tfPath, tgPath, _ = getWalk("/Users/idong-gyu/dev/github-actions/terradrift/__test__", tfPath, tgPath)
-
-	fmt.Println("terraform path >> ", tfPath)
-	fmt.Println("terragrunt path >> ", tgPath)
-
-	assert.Equal(t, len(tfPath), 3)
-	assert.Equal(t, len(tgPath), 2)
 }
